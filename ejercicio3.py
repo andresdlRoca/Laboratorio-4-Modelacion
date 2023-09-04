2844dd2be88362ebeef16626a9a46bc631cd2d4d
jwlh00 <60154594+jwlh00@users.noreply.github.com>
2023-09-04 01:01:08 -0600

    Ejercicio3
    Added ejercicio3

---
+++ ejercicio3.py
@@ -0,0 +1,18 @@
+import numpy as np
+from scipy.special import erf
+
+def inverse_transform_sampling(u):
+    valid_range = 2*u/np.sqrt(np.pi) - 1
+    valid_range = np.clip(valid_range, 1e-10, 1.0)  # Ensure values are within [1e-10, 1]
+    return np.sqrt(-np.log(valid_range))
+
+def integral_estimate(N):
+    u_values = np.random.rand(N)
+    x_values = inverse_transform_sampling(u_values)
+    f_values = x_values * np.exp(-x_values**2)
+    estimate = np.mean(f_values)
+    return estimate
+
+N = 100000  # Number of samples
+result = integral_estimate(N)
+print("Estimated value of the integral:", result)
